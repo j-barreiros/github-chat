52a4359752b996118cfdc4103501ef5c8c9ce842
j-novaes <joaovictor.nb21@hotmail.com>
2022-11-12 20:58:46 +0000

    implemented realtime

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MessageInput from './components/MessageInput/MessageInput'
 
 
 //Supabase
-import {createClient} from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 const PROJECT_URL = 'https://wdmngefxwwygmivvtgok.supabase.co';
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbW5nZWZ4d3d5Z21pdnZ0Z29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyNjg1NDksImV4cCI6MTk4Mzg0NDU0OX0.9O9yDhbGvgkyoDSNvSgn-5CBY61FjDuR4NafRQGK7g0'
 const supabase = createClient(PROJECT_URL, API_KEY);
@@ -18,29 +18,35 @@ function App() {
 
   useEffect(() => {
     supabase.from('messages')
-    .select('*')
-    .then(({data}) => {
-      setMessages(data)
-    })
+      .select('*')
+      .then(({ data }) => {
+        setMessages(data)
+      })
   }, [])
 
-  
-  useEffect(() => {
-    const messagesSubscription = supabase.channel('*').on('INSERT', data => console.log(data)).subscribe();
-  },[])
+  supabase
+    .channel("*")
+    .on(
+      "postgres_changes",
+      { event: "*", schema: "public", table: "messages" },
+      (payload) => {
+        console.log("Change received!", setMessages([...messages, payload.new]));
+      }
+    )
+    .subscribe();
 
   function addMessage(newMessage) {
     //setMessages([...messages,newMessage]);
     supabase.from('messages')
-    .insert(newMessage)
-    .then()
-    .catch()
+      .insert(newMessage)
+      .then()
+      .catch()
   }
 
   return (
     <div className="App">
       <MessageBoard messages={messages} />
-      <MessageInput addMessage={addMessage}/>
+      <MessageInput addMessage={addMessage} />
     </div>
   )
 }
